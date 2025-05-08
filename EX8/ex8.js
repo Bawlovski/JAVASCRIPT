493772aee10243c3f83841e78827ccd8891b64c7
Bawlovski <124616339+Bawlovski@users.noreply.github.com>
2025-05-08 18:13:20 +0200

    .
    .

--- EX8/ex8.js
+++ EX8/ex8.js
@@ -1,19 +1,70 @@
-var x = 0;
+var x = 1;
+var y = 1;
 const main = document.getElementById("main");
+
+const images = [
+    document.getElementById('1'),
+    document.getElementById('2'),
+    document.getElementById('3'),
+];
     
     // Get image sources correctly
-const images = [
-    document.getElementById("1"),
-    document.getElementById("2"),
-    document.getElementById("3")
+const imagessrc = [
+    "wisp1.jpg",
+    "wisp2.jpg",
+    "wisp3.jpg",
+    "wisp4.jpg",
+    "wisp5.jpg",
+    "wisp6.jpg"
 ];
 
+
+function loadCustom() {
+    main.src= document.getElementById("2").src;
+    images[x].style.border = "solid 2px red";
+}
+
+function clicked(image){
+    images[x].style.border = "";
+    x = image.id -1;
+    main.src = images[x].src;
+    images[x].style.border = "solid 2px red";
+}
+
 function changeForward() {
+    
     if (x < images.length - 1) {  
         images[x].style.border = "";
         x++;
-        main.src = images[x].src;
+        y++;
+        main.src = imagessrc[y];
         images[x].style.border = "solid 2px red";
+    }else {
+        
+        if(x < imagessrc.length - 1 && y < imagessrc.length - 1) {
+            images[x].style.border = "";
+            x = 0;
+            
+            
+            images[0].src = imagessrc[y + 1];
+            images[1].src = imagessrc[y + 2];
+            images[2].src = imagessrc[y + 3];
+            y++;
+            main.src = imagessrc[y];
+            images[x].style.border = "solid 2px red";
+           
+        }else {
+            images[x].style.border = "";
+            x = 0;
+            y = 0;
+            
+            images[0].src = imagessrc[0];
+            images[1].src = imagessrc[1];
+            images[2].src = imagessrc[2];
+            main.src = imagessrc[0];
+            images[x].style.border = "solid 2px red"; 
+        }
+        
     }
 }
 
@@ -21,7 +72,39 @@ function changeBackward() {
     if (x > 0) {  
         images[x].style.border = "";
         x--;
-        main.src = images[x].src;
+        y--;
+        main.src = imagessrc[y];
         images[x].style.border = "solid 2px red";
+    }else {
+        
+        if(x <= 0 && y <= 0) {
+            y = imagessrc.length - 1;
+            x = images.length - 1;
+            images[0].style.border = "";
+            
+            
+            
+            images[2].src = imagessrc[y];
+            images[1].src = imagessrc[y- 1];
+            images[0].src = imagessrc[y - 2];
+            
+            main.src = imagessrc[y];
+            images[x].style.border = "solid 2px red";
+           
+        }else {
+            x = images.length - 1;
+            y--;
+            
+            
+            images[0].style.border = "";
+            
+            
+            images[0].src = imagessrc[y - 2];
+            images[1].src = imagessrc[y - 1];
+            images[2].src = imagessrc[y];
+            main.src = imagessrc[x];
+            images[x].style.border = "solid 2px red"; 
+        }
+        
     }
 }
